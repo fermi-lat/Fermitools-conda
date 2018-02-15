4dbef127a4cdc2a2d0d80ee72b6b48366296f64f
Alex Reustle <areustledev@gmail.com>
2018-02-14 20:21:49 -0800

    swap link order

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir ${PREFIX}/include/fftw
 ln -s ${PREFIX}/include/fftw3.* ${PREFIX}/include/fftw
 
 # link to tinfo instead of termcap (provides the same functions)
-ln -s ${PREFIX}/lib/libtermcap.so ${PREFIX}/lib/libtinfo.so
+ln -s ${PREFIX}/lib/libtinfo.so ${PREFIX}/lib/libtermcap.so
 
 scons -C ScienceTools --site-dir=../SConsShared/site_scons --conda=${PREFIX} \
   --with-cxx=`which gcc`\
