d12ddebbc2b81811cd5241053c217d0e8e5f514e
Alex Reustle <areustledev@gmail.com>
2018-02-14 17:32:41 -0800

    fftw prefix

--- build.sh
+++ build.sh
@@ -5,7 +5,13 @@ repoman --remote-base https://github.com/fermi-lat checkout --force --develop Sc
 mkdir ${PREFIX}/include/fftw
 ln -s ${PREFIX}/include/fftw3.* ${PREFIX}/include/fftw
 
+
+
 scons -C ScienceTools --site-dir=../SConsShared/site_scons --conda=${PREFIX} \
   --with-cxx=`which gcc`\
   --with-cc= `which gcc`\
   all
+
+
+# link to tinfo instead of termcap (provides the same functions)
+# add
