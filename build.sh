f76a713c627ac39900621f99d395e47cb4591bd7
Alex Reustle <areustledev@gmail.com>
2018-02-14 16:34:31 -0800

    remote base

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-repoman checkout --force --develop ScienceTools conda
+repoman --remote-base https://github.com/fermi-lat checkout --force --develop ScienceTools conda
 
 scons -C ScienceTools --site-dir=../SConsShared/site_scons --conda=${PREFIX} all
