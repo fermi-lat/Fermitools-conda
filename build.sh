bd6bf3c074e87173bef525b9a58a5baa54bfc096
Alex Reustle <areustledev@gmail.com>
2018-02-21 13:23:10 -0500

    Improve comment on repoman syntax

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 export condaname="fermitools"
 
+# REPOMAN! #
+# Syntax Help:
+# To checkout master instead of the release tag add '--develop' after checkout
+# To checkout arbitrary other refs (Tag, Branch, Commit) add them as a space
+#   delimited list after 'conda' in the order of priority.
+#   e.g. ScienceTools highest_priority_commit middle_priority_ref branch1 branch2 ... lowest_priority
 repoman --remote-base https://github.com/fermi-lat checkout --force ScienceTools conda
 
 # condaforge fftw is in a different spot
