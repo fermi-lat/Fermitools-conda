cb5c8a038e153c6cba370ddc5add94619c2a4fdb
Alex Reustle <Areustle@users.noreply.github.com>
2018-09-05 15:29:05 -0400

    Check $PATH before appending to it (#5)
    * Check $PATH before appending to it
    
    * Use explicit variable reference for PFILES
    
    * Revert "Use explicit variable reference for PFILES"
    
    This reverts commit 77f08053a6ffb4367b909a9fe0e612c5590629cd.
    
    * Revert "Revert "Use explicit variable reference for PFILES""
    
    This reverts commit 56b12fd70b407a10c3a68441c07ed5f673ec2836.

--- activate.sh
+++ activate.sh
@@ -16,12 +16,22 @@ export GENERICSOURCESDATAPATH=$CONDA_PREFIX/share/${condaname}/data/genericSourc
 export TIMING_DIR=$CONDA_PREFIX/share/${condaname}/refdata/fermi/jplephem
 #export PFILES=$CONDA_PREFIX/share/${condaname}/syspfiles
 
-# Keep a copy of the current path so we can restore 
+# Keep a copy of the current path so we can restore
 # upon deactivation
 export FERMI_OLD_PATH=$PATH
 
+# The new path to check or add
+NEW_FERMI_PATH=$CONDA_PREFIX/bin/${condaname}
+
+# Check that the new path is not already a member of the $PATH
+if [[ ${PATH} != *"${NEW_FERMI_PATH}"* ]]; then
+
+    # Add the new fermi path to the $PATH
+    export PATH=${NEW_FERMI_PATH}:${PATH}
+
+fi
+
 # Add path for the ST binaries
-export PATH=$CONDA_PREFIX/bin/${condaname}:${PATH}
 
 # Setup PFILES
 
@@ -29,30 +39,30 @@ export PATH=$CONDA_PREFIX/bin/${condaname}:${PATH}
 # PFILES is not set)
 export FERMI_OLD_PFILES=$PFILES
 
-if [ -z ${PFILES+x} ]; then 
-    
+if [ -z ${PFILES+x} ]; then
+
     # PFILES is unset, set it appropriately
     mkdir -p $HOME/pfiles
-    
+
     export PFILES=".:${HOME}/pfiles;${INST_DIR}/syspfiles"
-    
-else 
-    
-    if [[ $1 == *[';']* ]]; then
-        
+
+else
+
+    if [[ ${PFILES} == *[';']* ]]; then
+
         # current value already contains a ';', which
         # separates read-write pfiles path to read-only
         # pfiles path. Just add the ST one
-        
+
         export PFILES="${PFILES}:${INST_DIR}/syspfiles"
-    
+
     else
-        
+
         # Current value doesn't have any read-only
         # pfiles path. Add the ST one.
-        
+
         export PFILES="${PFILES};${INST_DIR}/syspfiles"
-    
+
     fi
 
 
@@ -61,7 +71,7 @@ fi
 # Make sure there is no ROOTSYS (which would confuse ROOT)
 unset ROOTSYS
 
-# We need to add the path to the ST library dir to the 
+# We need to add the path to the ST library dir to the
 # path that ROOT will search for libraries, because ROOT
 # does not honor RPATH
 
@@ -69,8 +79,8 @@ cat << EOF | root -b -l
 
 TString old_value=gEnv->GetValue("Unix.*.Root.DynamicPath", "hey");
 
-if(!old_value.Contains("lib/${condaname}")) 
-{ 
+if(!old_value.Contains("lib/${condaname}"))
+{
     TString new_value = old_value + TString(":${CONDA_PREFIX}/lib/${condaname}/");
 
     gEnv->SetValue("Unix.*.Root.DynamicPath", new_value);
@@ -92,23 +102,23 @@ alias ObsSim="python $sitepackagesdir/${condaname}/ObsSim.py"
 
 # Issue warnings if PYTHONPATH and/or LD_LIBRARY_PATH are set
 
-if [ -z ${LD_LIBRARY_PATH+x} ]; then 
-    
+if [ -z ${LD_LIBRARY_PATH+x} ]; then
+
     :
 
 else
-    
+
     # Issue warning
     echo "You have LD_LIBRARY_PATH set. This might interfere with the correct functioning of conda and the Fermi ST"
 
 fi
 
-if [ -z ${PYTHONPATH+x} ]; then 
-    
+if [ -z ${PYTHONPATH+x} ]; then
+
     :
 
 else
-    
+
     # Issue warning
     echo "You have PYTHONPATH set. This might interfere with the correct functioning of conda and the Fermi ST"
 
