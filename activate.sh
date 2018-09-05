1faa93b72e946837ec51ce66678a4788f2082359
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-09-05 13:40:57 -0700

    Fixing some typos

--- activate.sh
+++ activate.sh
@@ -2,22 +2,36 @@
 
 export condaname="fermitools"
 
+function string_replace {
+    echo "${1/\*/$2}"
+}
 
 # This instructs the Fermi ST where to find their data
 
 export INST_DIR=$CONDA_PREFIX/share/${condaname}
+export FERMI_DIR=$INST_DIR
 export BASE_DIR=$INST_DIR
 export EXTFILESSYS=$CONDA_PREFIX/share/${condaname}/refdata/fermi
 export GENERICSOURCESDATAPATH=$CONDA_PREFIX/share/${condaname}/data/genericSources
 export TIMING_DIR=$CONDA_PREFIX/share/${condaname}/refdata/fermi/jplephem
 #export PFILES=$CONDA_PREFIX/share/${condaname}/syspfiles
 
-# Keep a copy of the current path so we can restore 
+# Keep a copy of the current path so we can restore
 # upon deactivation
 export FERMI_OLD_PATH=$PATH
 
+# The new path to check or add
+NEW_FERMI_PATH=$CONDA_PREFIX/bin/${condaname}
+
+# Check that the new path is not already a member of the $PATH
+if [[ ${PATH} != *"${NEW_FERMI_PATH}"* ]]; then
+
+    # Add the new fermi path to the $PATH
+    export PATH=${NEW_FERMI_PATH}:${PATH}
+
+fi
+
 # Add path for the ST binaries
-export PATH=$CONDA_PREFIX/bin/${condaname}:${PATH}
 
 # Setup PFILES
 
@@ -25,30 +39,30 @@ export PATH=$CONDA_PREFIX/bin/${condaname}:${PATH}
 # PFILES is not set)
 export FERMI_OLD_PFILES=$PFILES
 
-if [ -z ${PFILES+x} ]; then 
-    
+if [ -z ${PFILES+x} ]; then
+
     # PFILES is unset, set it appropriately
     mkdir -p $HOME/pfiles
-    
-    export PFILES=".:${HOME}/pfiles:${INST_DIR}/syspfiles"
-    
-else 
-    
-    if [[ $1 == *[';']* ]]; then
-        
+
+    export PFILES=".:${HOME}/pfiles;${INST_DIR}/syspfiles"
+
+else
+
+    if [[ ${PFILES} == *[';']* ]]; then
+
         # current value already contains a ';', which
         # separates read-write pfiles path to read-only
         # pfiles path. Just add the ST one
-        
-        export PFILES="${PFILES};${INST_DIR}/syspfiles"
-    
+
+        export PFILES="${PFILES}:${INST_DIR}/syspfiles"
+
     else
-        
+
         # Current value doesn't have any read-only
         # pfiles path. Add the ST one.
-        
+
         export PFILES="${PFILES};${INST_DIR}/syspfiles"
-    
+
     fi
 
 
@@ -97,7 +111,6 @@ exit();
 
 EOF
 
-
 # Add aliases for python executables
 sitepackagesdir=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 
@@ -107,25 +120,24 @@ alias ObsSim="python $sitepackagesdir/${condaname}/ObsSim.py"
 
 # Issue warnings if PYTHONPATH and/or LD_LIBRARY_PATH are set
 
-if [ -z ${LD_LIBRARY_PATH+x} ]; then 
-    
+if [ -z ${LD_LIBRARY_PATH+x} ]; then
+
     :
 
 else
-    
+
     # Issue warning
     echo "You have LD_LIBRARY_PATH set. This might interfere with the correct functioning of conda and the Fermi ST"
 
 fi
 
-if [ -z ${PYTHONPATH+x} ]; then 
-    
+if [ -z ${PYTHONPATH+x} ]; then
+
     :
 
 else
-    
+
     # Issue warning
     echo "You have PYTHONPATH set. This might interfere with the correct functioning of conda and the Fermi ST"
 
 fi
-
