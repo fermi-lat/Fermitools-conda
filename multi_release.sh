c0841e49de06766cb6c340bd9f13bda301d240d7
Alex Reustle <areustledev@gmail.com>
2020-06-24 14:12:19 -0400

    alternate tagging method

--- multi_release.sh
+++ multi_release.sh
@@ -23,14 +23,16 @@ EOF
 for f in *; do
     echo "Tagging $f..."
     cd $f
-    version=$1
-    text=$2
-    branch=$(git rev-parse --abbrev-ref HEAD)
-    repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
-    token=$(git config --global github.token)
+    # version=$1
+    # text=$2
+    # branch=$(git rev-parse --abbrev-ref HEAD)
+    # repo_full_name=$(git config --get remote.origin.url | sed 's/.*:\/\/github.com\///;s/.git$//')
+    # token=$(git config --global github.token)
 
-    echo "Create release $version for repo: $repo_full_name branch: $branch"
-    echo "curl --data $(generate_post_data) https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
-    curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+    # echo "Create release $version for repo: $repo_full_name branch: $branch"
+    # echo "curl --data $(generate_post_data) https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+    # curl --data "$(generate_post_data)" "https://api.github.com/repos/$repo_full_name/releases?access_token=$token"
+    git tag -m "$2" $1
+    git push --tags
     cd ../
 done
