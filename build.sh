bfa2483094d1985deeab7dc4c86ceb9b670e674d
jasercion <jasercion.gwu@gmail.com>
2022-01-18 16:11:59 -0500

    remove CONDA_BUILD_SYSROOT preamble

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ export CFLAGS="${CFLAGS}"
 export CXXFLAGS="${CXXFLAGS}
 
 # Add rpaths needed for our compilation
-export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib/${condaname}:${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib:${CONDA_BUILD_SYSROOT}/usr/local/lib"
+#export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib/${condaname}:${PREFIX}/lib:${CONDA_BUILD_SYSROOT}/usr/lib:${CONDA_BUILD_SYSROOT}/usr/local/lib"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib/${condaname}:${PREFIX}/lib:/usr/lib:/usr/local/lib"
 
 if [ "$(uname)" == "Darwin" ]; then
 
