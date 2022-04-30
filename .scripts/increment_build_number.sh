9c4e1ac50f5153513ed383924690bd80d26e8942
Alex Reustle <areustledev@gmail.com>
2022-04-30 11:33:39 -0400

    Increment Build Number fix

--- .scripts/increment_build_number.sh
+++ .scripts/increment_build_number.sh
@@ -1,9 +1,13 @@
-export VERSION=$(cat ${RECIPE_DIR}/meta.yaml | \
+export FERMITOOLS_VERSION=$(cat ${RECIPE_DIR}/meta.yaml | \
   grep '{% set version =' | awk '{ print $5 }' | tr -d \")
+
+echo -e "Fermitools Version: ${FERMITOOLS_VERSION}"
 
 export BUILD_NUMBER="${BUILD_NUMBER:-$(conda search \
   -c fermi/label/dev/osx-64 \
   -c fermi/label/dev/osx-arm64 \
   -c fermi/label/dev/linux-aarch64 \
   -c fermi/label/dev/linux-64 \
-  fermitools=${VERSION} --info --json | jq -r '.fermitools | max .build_number+1')}"
+  fermitools=${FERMITOOLS_VERSION} --info --json | jq -r '.fermitools | (max .build_number)+1')}"
+
+echo -e "Version Build Number: ${BUILD_NUMBER}"
