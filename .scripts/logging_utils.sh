a6484a9df2d6d8f5cc45f2d2d5247d4a1f882736
Alex Reustle <areustledev@gmail.com>
2022-05-11 16:21:19 -0400

    Only tag ScienceTools repos

--- .scripts/logging_utils.sh
+++ .scripts/logging_utils.sh
@@ -5,9 +5,31 @@
 # a compatible fallback (e.g. bare `echo xxxxxx`).
 
 function startgroup {
-    echo "##[group]$1"
+    # Start a foldable group of log lines
+    # Pass a single argument, quoted
+    case ${CI:-} in
+        azure )
+            echo "##[group]$1";;
+        travis )
+            echo "$1"
+            echo -en 'travis_fold:start:'"${1// /}"'\\r';;
+        github_actions )
+            echo "::group::$1";;
+        * )
+            echo "$1";;
+    esac
 } 2> /dev/null
 
 function endgroup {
-    echo "##[endgroup]"
+    # End a foldable group of log lines
+    # Pass a single argument, quoted
+
+    case ${CI:-} in
+        azure )
+            echo "##[endgroup]";;
+        travis )
+            echo -en 'travis_fold:end:'"${1// /}"'\\r';;
+        github_actions )
+            echo "::endgroup::";;
+    esac
 } 2> /dev/null
