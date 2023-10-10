0445caea65b6ea6068c8b3cd8ab9c104890bbbbd
jasercion <jasercion.gwu@gmail.com>
2023-10-10 15:28:28 -0400

    remove debug statement

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -45,14 +45,6 @@ if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
     EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
 fi
 
-######
-echo "PYTHONPATH: "
-echo $PYTHONPATH
-echo "which python: "
-which python
-echo "python version: "
-python --version
-
 conda mambabuild \
   -c fermi \
   -c conda-forge \
