fc66a23e48b7f79c4bbdbb16217f992aecfa429f
jasercion <jasercion.gwu@gmail.com>
2024-11-15 19:05:51 -0500

    update debug print

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ if [ "$(uname)" == "Darwin" ]; then
     export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
     export TOOLCHAIN_FILE="${RECIPE_DIR}/toolchain/cross-osx.cmake"
     echo "Commandline tools:" 
-    ls /Library/Developer/CommandLineTools/SDKs/
+    ls /Applications/Xcode_15.4.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/include/c++/v1/
 else
     export TOOLCHAIN_FILE="${RECIPE_DIR}/toolchain/cross-linux.cmake"
 fi
